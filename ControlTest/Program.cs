e1a0ad73a826249889ab714c077c31152e5c1dbc
olga <bagira_to@mail.ru>
2022-10-05 12:32:22 +0300

    Добавила решение итоговой контрольной работы

---
+++ ControlTest/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string[] mymassive = new string[4] {"777", "hi", "hello", "324"};
+DecisionTask(mymassive);
+
+void DecisionTask(string[] mymassive)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < mymassive.Length; i++)
+    {
+        if(mymassive[i].Length <= 3)
+        {
+            Console.Write($"{mymassive[i]} ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
